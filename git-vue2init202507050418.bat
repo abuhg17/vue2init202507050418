1919a6a5a861688e941998f4f0f9e36676d2f112
goldshoot0720 <goldshoot0720@gmail.com>
2025-06-19 16:15:30 +0800

    202507050418

---
+++ git-vue2init202507050418.bat
@@ -0,0 +1,12 @@
+git init
+
+git add .
+
+git commit -m "202507050418"
+
+git remote add origin https://github.com/abuhg17/vue2init202507050418.git
+
+git checkout -b main
+git push -u origin main --force
+
+pause
---
+++ open-vue2init202507050418.bat
@@ -0,0 +1,3 @@
+cd "C:\Users\chbon\Documents\20250705\vue\vue2init202507050418"
+explorer http://localhost:5173/
+npm run dev
